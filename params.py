76bd8b138a35add429fdc51aaef8716c30769fb4
Guangwei WANG <wgw0415@gmail.com>
2017-04-02 18:27:36 +0800

    feat: Change benchmark model
    Change benchmark model to Nvidia model, add YUV data processing mode

--- params.py
+++ params.py
@@ -1,11 +1,10 @@
-import os
-from collections import OrderedDict
 
+import os
 import numpy as np
 import tensorflow as tf
 
-#### CONST
-## Initialize Constant
+# CONST
+# Initialize Constant
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
@@ -14,15 +13,15 @@ img_height = 66
 img_width = 200
 img_channels = 3
 
-## Nvida's camera format
+# Nvida's camera format
 flags.DEFINE_integer('img_h', 66, 'The image height.')
 flags.DEFINE_integer('img_w', 200, 'The image width.')
 flags.DEFINE_integer('img_c', 3, 'The number of channels.')
 
-## Fix random seed for reproducibility
+# Fix random seed for reproducibility
 np.random.seed(42)
 
-## Path
+# Path
 data_dir = os.path.abspath('./epochs')
 out_dir = os.path.abspath('./output')
 model_dir = os.path.abspath('./models')
--- run.py
+++ run.py
@@ -1,58 +1,63 @@
-#!/usr/bin/env python 
+
+#!/usr/bin/env python
 import sys
 import os
 import time
 import subprocess as sp
 import itertools
-## CV
+# CV
 import cv2
-## Model
+# Model
 import numpy as np
-import tensorflow as tf
-## Tools
+# Tools
 import utils
 
-## Parameters
-import params ## you can modify the content of params.py
+# Parameters
+import params  # you can modify the content of params.py
 import preprocess
 img_height = params.img_height
 img_width = params.img_width
 img_channels = params.img_channels
 
 
-## Test epoch
+# Test epoch
 epoch_ids = [10]
-## Load model
+# Load model
 model = utils.get_model()
 
-## Preprocess
+# Preprocess
+
+
 def img_pre_process(img):
     """
     Processes the image and returns it
     :param img: The image to be processed
     :return: Returns the processed image
-    """ 
-    # Chop off 1/2 from the top and cut bottom 150px(which contains the head of car)
+    """
+    # Chop off 1/2 from the top and cut bottom 150px(which contains the head
+    # of car)
     ratio = img_height / img_width
-    h1, h2 = int(img.shape[0]/2),img.shape[0]-150
-    w = (h2-h1) / ratio
+    h1, h2 = int(img.shape[0] / 2), img.shape[0] - 150
+    w = (h2 - h1) / ratio
     padding = int(round((img.shape[1] - w) / 2))
     img = img[h1:h2, padding:-padding]
-    ## Resize the image
-    img = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_AREA)
-    ## Image Normalization
-    #img = img / 255. 
+    # Resize the image
+    img = cv2.resize(img, (img_width, img_height),
+                     interpolation=cv2.INTER_AREA)
+    # Image Normalization
+    #img = img / 255.
     return img
 
-## Process video
+
+# Process video
 for epoch_id in epoch_ids:
     print('---------- processing video for epoch {} ----------'.format(epoch_id))
-    vid_path = utils.join_dir(params.data_dir, 'epoch{:0>2}_front.mkv'.format(epoch_id))
+    vid_path = utils.join_dir(
+        params.data_dir, 'epoch{:0>2}_front.mkv'.format(epoch_id))
     assert os.path.isfile(vid_path)
     cap = cv2.VideoCapture(vid_path)
 
-
-    # frame_count = utils.frame_count(vid_path) 
+    # frame_count = utils.frame_count(vid_path)
     # this line goes wrong, instead with below code
     ############ New codes added by student ############
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -69,7 +74,7 @@ for epoch_id in epoch_ids:
     print('performing inference...')
     time_start = time.time()
 
-    #for frame_id in range(frame_count):
+    # for frame_id in range(frame_count):
     #    ret, img = cap.read()
     #    assert ret
     # ## you can modify here based on your model
@@ -84,11 +89,10 @@ for epoch_id in epoch_ids:
     ####################################################
 
     fps = frame_count / (time.time() - time_start)
-    
-    print('completed inference, total frames: {}, average fps: {} Hz'.format(frame_count, round(fps, 1)))
-    
+
+    print('completed inference, total frames: {}, average fps: {} Hz'.format(
+        frame_count, round(fps, 1)))
+
     print('performing visualization...')
     utils.visualize(epoch_id, machine_steering, params.out_dir,
-                        verbose=True, frame_count_limit=None)
-    
-    
+                    verbose=True, frame_count_limit=None)
